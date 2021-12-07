e3bd02ec67032cb7c76c9984a8bbd8e2f651702b
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-12-07 11:01:41 -0600

    working through sequence function

--- game.js
+++ game.js
@@ -3,50 +3,51 @@
 // global variables
 let gamePattern = [];
 let buttonColors = ["red", "blue", "green", "yellow"];
+let randomChosenColor = buttonColors[nextSequence()];
+gamePattern.push(randomChosenColor);
+$('#' + randomChosenColor).fadeOut(100).fadeIn(100);
+
 let redSound = new Audio("sounds/red.mp3");
 let blueSound = new Audio("sounds/blue.mp3");
 let greenSound = new Audio("sounds/green.mp3");
 let yellowSound = new Audio("sounds/yellow.mp3");
 let wrongSound = new Audio("sounds/wrong.mp3");
+let userClickedPattern = [];
 
 // random color generator for sequencing
 function nextSequence() {
 	let randomNum = Math.floor(Math.random() * 4) + 0;
-	let randomChosenColor = buttonColors[randomNum];
-	gamePattern.push(randomChosenColor);
-	console.log(randomChosenColor)
-	return randomChosenColor;
+	return randomNum;
 }
 
 // function to play sound for appropriate color
 function playSound(color) {
 	switch (color) {
 		case "red":
-			redSound.play().then(r => {
-				console.log("red")});
+			redSound.play();
 			break;
 		case "blue":
-			blueSound.play().then(r => {
-				console.log("blue")});
+			blueSound.play();
 			break;
 		case "green":
-			greenSound.play().then(r => {
-				console.log("green")});
+			greenSound.play();
 			break;
 		case "yellow":
-			yellowSound.play().then(r => {
-				console.log("yellow")});
+			yellowSound.play();
 			break;
 		default:
-			wrongSound.play().then(r => {
-				console.log(color)})
-			// console.log(color);
+			wrongSound.play();
 	}
 }
 
 // click event to play sound
 $('div').click(function (e) {
-	console.log(e);
+	let userChosenColor = e.target.id;
+	// console.log(e);
 	playSound(e.target.id);
 	$('#' + e.target.id).fadeOut(100).fadeIn(100);
+	userClickedPattern.push(userChosenColor);
+	console.log(userClickedPattern);
 })
+
+// console.log(userClickedPattern);
