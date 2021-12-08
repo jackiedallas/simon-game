57c1d5197f8472db5cb42a6f82dea071c806bbc6
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-12-07 23:57:15 -0600

    added check answer function

--- game.js
+++ game.js
@@ -3,9 +3,6 @@
 // global variables
 let gamePattern = [];
 let buttonColors = ["red", "blue", "green", "yellow"];
-let randomChosenColor = buttonColors[nextSequence()];
-gamePattern.push(randomChosenColor);
-$('#' + randomChosenColor).fadeOut(100).fadeIn(100);
 
 let redSound = new Audio("sounds/red.mp3");
 let blueSound = new Audio("sounds/blue.mp3");
@@ -13,11 +10,23 @@ let greenSound = new Audio("sounds/green.mp3");
 let yellowSound = new Audio("sounds/yellow.mp3");
 let wrongSound = new Audio("sounds/wrong.mp3");
 let userClickedPattern = [];
+let level = 0;
+let gameStarted = false;
 
 // random color generator for sequencing
 function nextSequence() {
-	let randomNum = Math.floor(Math.random() * 4) + 0;
-	return randomNum;
+	// reset  user clicked pattern for next sequence
+	userClickedPattern = [];
+	
+	level++;
+	$('h1').html("Level " + level)
+	let randomNum = Math.floor(Math.random() * 4);
+	let randomChosenColor = buttonColors[randomNum];
+	gamePattern.push(randomChosenColor);
+	
+	$('#' + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+	playSound(randomChosenColor)
+	
 }
 
 // function to play sound for appropriate color
@@ -40,14 +49,44 @@ function playSound(color) {
 	}
 }
 
+// function to animate the pressed button
+function animatePress(currentColor) {
+	$('#' + currentColor).addClass('pressed');
+	setTimeout(function () {
+		$('#' + currentColor).removeClass('pressed');
+	}, 100)
+}
+
 // click event to play sound
-$('div').click(function (e) {
-	let userChosenColor = e.target.id;
-	// console.log(e);
-	playSound(e.target.id);
-	$('#' + e.target.id).fadeOut(100).fadeIn(100);
+$('.btn').click(function () {
+	let userChosenColor = $(this).attr("id");
+	playSound(userChosenColor);
+	animatePress(userChosenColor);
 	userClickedPattern.push(userChosenColor);
-	console.log(userClickedPattern);
+	checkAnswer(userClickedPattern[userClickedPattern -1 ])
+	// console.log(userClickedPattern);
 })
 
-// console.log(userClickedPattern);
+function checkAnswer(currentLevel) {
+	
+	// if statement to check if most recent answer is the same as game answer
+	if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+		console.log("success")
+		if (userClickedPattern.length === gamePattern.length) {
+			// call nextSequence after 1 second
+			setTimeout(() => {nextSequence()}, 1000);
+		}
+	} else {
+		console.log("wrong")
+	}
+}
+
+$(document).keypress(function () {
+	if (!gameStarted) {
+		$('h1').html("Level " + level);
+		nextSequence();
+		gameStarted = true;
+	}
+	// level++;
+})
+
